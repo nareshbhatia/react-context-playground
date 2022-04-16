62730e9cd0b08a223fbd1d7921b20fa221dca95a
Naresh Bhatia <naresh.archfirst@gmail.com>
2022-04-16 19:11:57 -0400

    Implemented edit mode on the sidebar

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -10,36 +10,12 @@ import { Widget } from '../../components';
 import { Sidebar } from './Sidebar';
 
 const widgets = [
-  {
-    title: 'Account Balance',
-    icon: ScaleIcon,
-    amount: '$30,211.28',
-  },
-  {
-    title: 'Pending',
-    icon: RefreshIcon,
-    amount: '-$19,500.00',
-  },
-  {
-    title: 'Processed',
-    icon: CheckCircleIcon,
-    amount: '$12,000.00',
-  },
-  {
-    title: 'Net Worth',
-    icon: CurrencyDollarIcon,
-    amount: '$52,568.22',
-  },
-  {
-    title: 'Investments',
-    icon: LibraryIcon,
-    amount: '$22,568.22',
-  },
-  {
-    title: 'Cash',
-    icon: CashIcon,
-    amount: '$30,000.00',
-  },
+  { title: 'Account Balance', icon: ScaleIcon, amount: '$30,211.28' },
+  { title: 'Pending', icon: RefreshIcon, amount: '-$19,500.00' },
+  { title: 'Processed', icon: CheckCircleIcon, amount: '$12,000.00' },
+  { title: 'Net Worth', icon: CurrencyDollarIcon, amount: '$52,568.22' },
+  { title: 'Investments', icon: LibraryIcon, amount: '$22,568.22' },
+  { title: 'Cash', icon: CashIcon, amount: '$30,000.00' },
 ];
 
 export function HomePage() {
@@ -60,7 +36,7 @@ export function HomePage() {
                       aria-hidden="true"
                     />
                   </div>
-                  <div className="ml-5 w-0 flex-1">
+                  <div className="ml-4 w-0 flex-1">
                     <div className="text-lg font-medium text-gray-900">
                       {widget.amount}
                     </div>
--- src/pages/HomePage/Sidebar.tsx
+++ src/pages/HomePage/Sidebar.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import {
   CalendarIcon,
   ChartBarIcon,
@@ -6,18 +7,48 @@ import {
   InboxIcon,
   UsersIcon,
 } from '@heroicons/react/outline';
+import { useViewStateContext } from '../../contexts';
+
+type NavItem = {
+  name: string;
+  icon: any;
+  current: boolean;
+  show: boolean;
+};
 
 export function Sidebar() {
   console.log('Sidebar.render');
+  const { viewState } = useViewStateContext();
+  const { isEditing } = viewState;
 
-  const navigation = [
-    { name: 'Dashboard', href: '#', icon: HomeIcon, current: true },
-    { name: 'Team', href: '#', icon: UsersIcon, current: false },
-    { name: 'Projects', href: '#', icon: FolderIcon, current: false },
-    { name: 'Calendar', href: '#', icon: CalendarIcon, current: false },
-    { name: 'Documents', href: '#', icon: InboxIcon, current: false },
-    { name: 'Reports', href: '#', icon: ChartBarIcon, current: false },
-  ];
+  const [navItems, setNavItems] = useState<Array<NavItem>>([
+    { name: 'Dashboard', icon: HomeIcon, current: true, show: true },
+    { name: 'Team', icon: UsersIcon, current: false, show: true },
+    { name: 'Projects', icon: FolderIcon, current: false, show: true },
+    { name: 'Calendar', icon: CalendarIcon, current: false, show: true },
+    { name: 'Documents', icon: InboxIcon, current: false, show: false },
+    { name: 'Reports', icon: ChartBarIcon, current: false, show: false },
+  ]);
+
+  const handleItemClicked = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    const itemName = e.currentTarget.id;
+    setNavItems(
+      navItems.map((item) => {
+        return item.name === itemName
+          ? { ...item, current: true }
+          : { ...item, current: false };
+      })
+    );
+  };
+
+  const handleCheckboxChanged = (e: React.FormEvent<HTMLInputElement>) => {
+    const itemName = e.currentTarget.name;
+    setNavItems(
+      navItems.map((item) => {
+        return item.name === itemName ? { ...item, show: !item.show } : item;
+      })
+    );
+  };
 
   function classNames(...classes: any[]) {
     return classes.filter(Boolean).join(' ');
@@ -29,29 +60,44 @@ export function Sidebar() {
         <div className="flex-1 flex flex-col pt-5 pb-4 overflow-y-auto">
           <nav className="flex-1" aria-label="Sidebar">
             <div className="px-2 space-y-1">
-              {navigation.map((item) => (
-                <a
-                  key={item.name}
-                  href={item.href}
-                  className={classNames(
-                    item.current
-                      ? 'bg-gray-200 text-gray-900'
-                      : 'text-gray-600 hover:bg-gray-50 hover:text-gray-900',
-                    'group flex items-center px-2 py-2 text-sm font-medium rounded-md'
-                  )}
-                >
-                  <item.icon
+              {navItems.map((item) =>
+                isEditing || item.show ? (
+                  <a
+                    key={item.name}
+                    id={item.name}
+                    href="#"
                     className={classNames(
                       item.current
-                        ? 'text-gray-500'
-                        : 'text-gray-400 group-hover:text-gray-500',
-                      'mr-3 h-6 w-6'
+                        ? 'bg-gray-200 text-gray-900'
+                        : 'text-gray-600 hover:bg-gray-50 hover:text-gray-900',
+                      'group flex items-center px-2 py-2 text-sm font-medium rounded-md'
                     )}
-                    aria-hidden="true"
-                  />
-                  {item.name}
-                </a>
-              ))}
+                    onClick={handleItemClicked}
+                  >
+                    {isEditing && (
+                      <input
+                        id={item.name}
+                        name={item.name}
+                        aria-describedby={item.name}
+                        type="checkbox"
+                        checked={item.show}
+                        className="mr-2 focus:ring-gray-500 h-4 w-4 text-gray-600 border-gray-300 rounded"
+                        onChange={handleCheckboxChanged}
+                      />
+                    )}
+                    <item.icon
+                      className={classNames(
+                        item.current
+                          ? 'text-gray-500'
+                          : 'text-gray-400 group-hover:text-gray-500',
+                        'mr-3 h-6 w-6'
+                      )}
+                      aria-hidden="true"
+                    />
+                    {item.name}
+                  </a>
+                ) : null
+              )}
             </div>
           </nav>
         </div>
